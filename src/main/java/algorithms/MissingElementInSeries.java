2f19805faceb8474f5c3e9469dea0d9f2770de7b
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-07-26 03:05:24 +0200

    missing series element

---
+++ src/main/java/algorithms/MissingElementInSeries.java
@@ -0,0 +1,23 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MissingElementInSeries {
+
+    public static int findMissingElement(Integer[] inputArray) {
+        List<Integer> series = new ArrayList(Arrays.asList(inputArray));
+        int expectedSeriesSum = (series.size() * (series.size()+1) ) / 2;
+        int realSeriesSum = series.stream()
+                .mapToInt(e -> e)
+                .sum();
+
+        return expectedSeriesSum - realSeriesSum;
+    }
+
+    public static void main(String[] args) {
+        Integer[] testArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10}; //Missing number is 9
+        System.out.println("Missing number is: " + findMissingElement(testArray));
+    }
+}
